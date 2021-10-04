9ec9d7fba5fe72c850d661f0038de5cc03208974
backfromsolaris <porraszach@gmail.com>
2021-10-04 16:08:58 -0500

    initial boilerplate set

--- policy_lambda.py
+++ policy_lambda.py
@@ -4,13 +4,27 @@ print('Loading function')
 
 def lambda_handler(event, context):
     #1. parse out query string params
-    transactionID = event['queryStringParameters']['transactionID']
+    transactionId = event['queryStringParameters']['transactionId']
     transactionType = event['queryStringParameters']['type']
     transactionAmount = event['queryStringParameters']['amount']
 
-    print('transactionID=' + transactionID)
+    print('transactionId=' + transactionId)
     print('transactionType=' + transactionType)
     print('transactionAmount=' + transactionAmount)
 
     #2. construct body of response object
-    
+    transactionResponse = {}
+    transactionResponse['transactionId'] = transactionId
+    transactionResponse['type'] = transactionType
+    transactionResponse['amount'] = transactionAmount
+    transactionResponse['message'] = 'This is Simplified Insurance, we read you'
+
+    #3. construct http response object
+    responseObject = {}
+    responseObject['statusCode'] = 200
+    responseObject['headers'] = {}
+    responseObject['headers']['Content-Type'] = 'application/json'
+    responseObject['body'] = json.dumps(transactionResponse)
+
+    #4. return response object
+    return responseObject
